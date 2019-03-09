3b792aaea4618e7c670ec2db580d977068594900
janekot87 <janekot87@gmail.com>
2019-03-09 15:52:40 +0200

    Homqwork2

---
+++ ConsoleApp2/Market.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Market
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int SportId { get; set; }
+        public int LeagueId { get; set; }
+        public List<Selection> Selections;
+
+        public List<Selection> list = new List<Selection>();
+
+        public int Capasity;
+
+        public Market()
+        {
+        }
+
+
+
+        public Market(int c)
+        {
+
+            c = Capasity;
+        }
+
+
+        public void AddSelection(Selection value)
+        {
+
+
+            if (list.Count <= Capasity)
+            {
+               list.Add(value);
+            }
+            else
+            {
+                Console.WriteLine("you have reached the selection capacity");
+            }
+
+
+        }
+    }
+
+
+
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,15 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            
+
+
+                var sel1 = Selection.GetSelection();
+                var sel2 = Selection.GetSelection();
+                var sel3 = Selection.GetSelection();
+                var sel4 = Selection.GetSelection();
+
+                var mrk1 = new Market(3);
+                mrk1.Id = 1;
+                mrk1.Name = "Market1";
+                mrk1.SportId = 1;
+                mrk1.LeagueId = 1;
+                mrk1.AddSelection(sel1);
+                mrk1.AddSelection(sel2);
+                mrk1.AddSelection(sel3);
+                mrk1.AddSelection(sel4);
+
+            foreach (var i in mrk1.Selections)
+            {
+                Console.WriteLine(i.Id);
+                Console.WriteLine(i.Name);
+                Console.WriteLine(i.Title);
+                Console.WriteLine(i.IsDisabled);
+                Console.WriteLine(i.Status);
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+       
         }
     }
-}
+
